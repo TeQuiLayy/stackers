df85f00696919cfc74f46fd42d605ad92f5dda9e
tequila <yuya.arukat@gmail.com>
2018-04-19 23:15:33 +0900

    feat: Implement cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180413111055) do
+ActiveRecord::Schema.define(version: 20180419131254) do
 
   create_table "subjects", force: :cascade do |t|
     t.string "name"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20180413111055) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
